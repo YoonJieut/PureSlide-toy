3914bade0e8a671ce491073c1b112bf4432ec2e1
Yoonjieut <qmdldos@gmail.com>
2023-10-11 15:55:59 +0900

    leftmoving setInterval 완성, 변수를 setInterval안에 두지 말 것.

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ console.log('hi');
 const outbox = document.getElementById('ItemsOutbox');
 const slideUl = document.getElementById('slideUl');
 console.log(outbox, slideUl);
+console.dir(slideUl);
 
 // * 스르륵 -700 이동하는 것
-setInterval(()=>{
-  console.log('인터벌 시작 10');
-  console.log( slideUl.style.left);
-  let leftPx = 0;
-  if(leftPx > -700){
-    leftPx--;
-    slideUl.style.left = leftPx+"px";
-    console.log( slideUl.style.left);
-  }
-}, 10)
+function leftMove(){
+  console.log('왼쪽 이동 인터벌 시작')
+  let temp = 0;
+  let leftMoving = setInterval(()=>{
+    console.log("인터벌 도는 중");
+    if(temp > -700) {
+      temp = temp -7;
+      slideUl.style.left = temp+"px";
+    } else {
+      clearInterval(leftMoving)
+    }
+  }, 1)
+}
+
