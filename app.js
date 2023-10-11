45c1476316b0d48168de99fdeaef06b58e7a682f
Yoonjieut <qmdldos@gmail.com>
2023-10-11 17:30:08 +0900

    최종 css 수정본

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 console.log('hi');
 const outbox = document.getElementById('ItemsOutbox');
 const slideUl = document.getElementById('slideUl');
-console.log(outbox, slideUl);
-console.dir(slideUl);
 
-// * 스르륵 -700 이동하는 것
-let leftMove=() => {
-  console.log('왼쪽 이동 인터벌 시작')
+// * 스르륵 width 만큼 왼쪽으로 이동하는 것
+let leftMove= (width) => {
   let temp = 0;
   let leftMoving = setInterval(()=>{
-    console.log("인터벌 도는 중");
-    if(temp > -700) {
+    if(temp > -width) {
       temp = temp -7;
       slideUl.style.left = temp+"px";
     } else {
@@ -19,8 +15,10 @@ let leftMove=() => {
   }, 0.5)
 }
 
+// ! 첫 요소 삭제 및 뒤로 이동
 let appnedSomething = ()=>{
   console.log('append 실행됨')
+  // 첫 번째 요소 삭제하는 변수 작성
   let remove1st = slideUl.removeChild(slideUl.children[0]);
   // 삭제한 것을 반환하는 리무브 차일드 메소드 이용
   slideUl.appendChild(remove1st);
@@ -29,13 +27,12 @@ let appnedSomething = ()=>{
 }
 
 // ! 최종 setInterval 작성
-
 setTimeout(()=>{
   // 초반 5초 대기
   // 메인 슬라이드 시작
   setInterval(()=>{
     // *움직이고
-    leftMove();
+    leftMove(700);
     // * 기다리고 
     setTimeout(()=>{
       // *안 보이게 넘기고
