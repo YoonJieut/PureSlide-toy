f58b4adfe035c7e7807aa4f35d83bdaa2bd8bf01
Yoonjieut <qmdldos@gmail.com>
2023-10-11 16:46:53 +0900

    최종 로직 합체, 위치 초기화 잊지 말 것

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ console.log(outbox, slideUl);
 console.dir(slideUl);
 
 // * 스르륵 -700 이동하는 것
-function leftMove(){
+let leftMove=() => {
   console.log('왼쪽 이동 인터벌 시작')
   let temp = 0;
   let leftMoving = setInterval(()=>{
@@ -16,15 +16,32 @@ function leftMove(){
     } else {
       clearInterval(leftMoving)
     }
-  }, 1)
+  }, 0.5)
 }
 
-function appnedSomething (){
+let appnedSomething = ()=>{
   console.log('append 실행됨')
   let remove1st = slideUl.removeChild(slideUl.children[0]);
   // 삭제한 것을 반환하는 리무브 차일드 메소드 이용
   slideUl.appendChild(remove1st);
+  // ! 옮김과 동시에 left 0 초기화
+  slideUl.style.left = 0;
 }
-appnedSomething();
 
-setTimeout
+// ! 최종 setInterval 작성
+
+setTimeout(()=>{
+  // 초반 5초 대기
+  // 메인 슬라이드 시작
+  setInterval(()=>{
+    // *움직이고
+    leftMove();
+    // * 기다리고 
+    setTimeout(()=>{
+      // *안 보이게 넘기고
+      appnedSomething();
+    },1000);
+  }, 5000)
+},5000)
+
+
