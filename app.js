ffb61fa8692a4f9c531b0725878b290436e5b92f
Yoonjieut <qmdldos@gmail.com>
2023-10-11 15:43:55 +0900

    움직이는 setInterval 함수 제작

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('hi');
+const outbox = document.getElementById('ItemsOutbox');
+const slideUl = document.getElementById('slideUl');
+console.log(outbox, slideUl);
+
+// * 스르륵 -700 이동하는 것
+setInterval(()=>{
+  console.log('인터벌 시작 10');
+  console.log( slideUl.style.left);
+  let leftPx = 0;
+  if(leftPx > -700){
+    leftPx--;
+    slideUl.style.left = leftPx+"px";
+    console.log( slideUl.style.left);
+  }
+}, 10)
